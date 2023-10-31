d75ae71b4cf35fb8f3ce3fc9f5de5a7cf9a685be
Oscar Lee <wakie9292@gmail.com>
2023-10-31 19:26:56 +0900

    sambpleama

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
+import dynamic from 'next/dynamic';
 import React from 'react';
 
 import Landing from 'src/components/Landing';
 import Layout from 'src/components/shared/Layout';
 
+const BottomSheet = dynamic(() => import('src/components/shared/BottomSheet'), { ssr: false });
+
 const Index = () => {
   return (
     <Layout>
       <Landing />
+      <BottomSheet />
     </Layout>
   );
 };
